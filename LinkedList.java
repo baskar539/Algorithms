ead0c614a07b2709f8f017d2ac9b4110e1139450
baskar539 <162519679+baskar539@users.noreply.github.com>
2024-06-06 13:46:14 +0100

    Add files via upload

---
+++ LinkedList.java
@@ -0,0 +1,37 @@
+class Node{
+	int data;
+    Node next;
+    Node(int data)
+    {
+    	this.data = data;
+        this.next = null;
+    }
+}
+public class LinkedList
+{
+		public static Node createNode(int Data)
+        {
+        	return new Node(Data);
+        }
+        public static void printList(Node node)
+        {	
+        	while(node!=null)
+            {
+            	System.out.print("node.data"+"->");
+                node= node.next;
+            }
+            System.out.println("NULL");
+        }
+
+          public static void main(String[] args) {
+          Node node1 = createNode(12);
+          Node node2= createNode(15);
+          Node node3 = createNode(30);
+          Node node4 = createNode(33);
+
+          node1.next = node2;
+          node2.next = node3;
+          node3.next = node4;
+          printList(node1);
+        }
+}
